28f607ff760c3a1baffafd8b0d579b981668af5e
jjorgeb <jorge.barraza@uchile.cl>
2025-04-17 15:55:04 -0400

    chore: deletes unused functions and imports

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-
-import os
-
 from setuptools import setup, find_packages
-
-def package_data(pkg, roots):
-    """Generic function to find package_data.
-
-    All of the files under each of the `roots` will be declared as package
-    data for package `pkg`.
-
-    """
-    data = []
-    for root in roots:
-        for dirname, _, files in os.walk(os.path.join(pkg, root)):
-            for fname in files:
-                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
-
-    return {pkg: data}
 
 setup(
     name="img_annotation",
