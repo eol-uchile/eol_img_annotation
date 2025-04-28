1bb546a25b1e851547d05fba472a82153cda0acb
jjorgeb <jorge.barraza@uchile.cl>
2025-04-28 10:09:52 -0400

    refactor: correct typo in xblock description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.0",
     author="Oficina EOL UChile",
     author_email="eol-ing@uchile.cl",
-    description="Xblock with a viewer for high-resolution zoomable images. It lets both students and instructor to add annotations over it.",
+    description="Xblock with a viewer for high-resolution zoomable images. It lets both students and instructors add annotations over it.",
     url="https://eol.uchile.cl",
     packages=find_packages(),
     install_requires=[
