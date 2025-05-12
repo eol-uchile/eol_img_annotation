6bb0cf4e4d1e1c5eb30a23e5d375403e1125c798
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-05-12 17:54:31 -0400

    chore: clean up test setup and unused configs
    - Removed LMS-specific customization from setup.cfg
    - Deleted unnecessary coverage setting related to migrations
    - Updated test.sh to remove redundant use of sed

--- .github/test.sh
+++ .github/test.sh
@@ -6,19 +6,12 @@ pip install --src /openedx/venv/src -e /openedx/requirements/app
 pip install pytest-cov genbadge[coverage]
 
 cd /openedx/requirements/app
-cp /openedx/edx-platform/setup.cfg .
-
-sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=img_annotation/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
 
 mkdir test_root
-cd test_root/
-ln -s /openedx/staticfiles .
-
-cd /openedx/requirements/app
+ln -s /openedx/staticfiles ./test_root/
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest img_annotation/tests.py
 
 rm -rf test_root
 
-echo "[run]\nomit = img_annotation/migrations/*" > .coverage
 genbadge coverage
